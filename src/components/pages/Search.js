609e8269728a4689e502d6b21f41f3c24575ad78
mickael <mickael.cecen@gmail.com>
2022-05-03 20:01:07 +0200

    search section almost done working on infinite scroll

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import '../../components-css/Search.css';
+import React, { useEffect, useState } from 'react';
 
 const Search = () => {
+
+  const [searchs, setSearch] = useState([]);
+  const [userSearchs, setUserSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+      const getSearch = async () => {
+          const searchFromServer = await fetchSearch()
+          setSearch(searchFromServer)
+      }
+      getSearch()
+  }, [userSearchs])
+
+  const fetchSearch = async () => {
+      const res = await fetch('https://gamergategg.herokuapp.com/api/games');
+      const data = await res.json();
+        return data
+  }
+  
+
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className='search__container'> 
+        <input type="text" value={userSearchs} onChange={e => setUserSearch(e.target.value)} className="search__input" placeholder='Search a game or a categories'/>
+        <div className='search__images'>
+        {searchs.map(search => (search.names.toLowerCase().includes(userSearchs.toLowerCase()) || search.categories.toLowerCase().includes(userSearchs.toLowerCase()) ? (
+                  <div key={search.id} className='search__image'>
+                      <img src={search.img_url} alt={search.id} key={search.id}  />
+                  </div>
+              ) : ('')))}
+        </div>
+      </div>
+    </div>
   )
 }
 
